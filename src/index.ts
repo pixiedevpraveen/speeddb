c0b76e7388d75b4bf9642deaa7eb89f48a1b7902
Praveen Yadav <praveenyadav7787@gmail.com>
2024-02-26 23:05:12 +0530

    feat(SpeedDb) add class SpeedDb with constructor and other 8 other methods including getCollection, addCollection, loadCollection and close

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
+import { EventEmitter } from "./EventEmitter"
+import { Collection } from "./collection"
+import { SDBError, invalidName } from "./error"
+import type { CollectionInitOptions, CollectionJson, DBEvType, DBEventHandlers, SpeedDbConstructorOptions, SpeedDbOptions } from "./types"
+import { isArray } from "./utils"
+
 /**-----------------------------------------------------
- *
+ * ```
  *  .d8888b.                                  888 8888888b.  888888b.   
  * d88P  Y88b                                 888 888  "Y88b 888  "88b  
  * Y88b.                                      888 888    888 888  .88P  
@@ -11,16 +17,82 @@
  *            888                                                       
  *            888                                                       
  *            888                                                       
- *
+ * ```
  * A document oriented tiny and fast offline-first js database that supports (and|or) queries, sorting and indexing
  *
  * @author Praveen yadav
  *
  * ------------------------------------------------------*/
+export class SpeedDb extends EventEmitter<DBEvType, DBEventHandlers> {
+    constructor(name?: string, opt?: Partial<SpeedDbConstructorOptions>) {
+        super(["init", "loaded", "changes", "save", "flushChanges", "close", "warning"])
+        this.name = name || "speed.db"
+        this.options.env = opt?.env || "BROWSER"
+    }
 
-class SpeedDb {
-    constructor() {
+    name: string
+    private collections = {} as Record<string, Collection<any>>
+    options: Partial<SpeedDbConstructorOptions> & SpeedDbOptions = {
+        autoload: true,
+        autosave: true,
+        autosaveInterval: 1000,
+        env: "BROWSER"
+    }
+
+    getCollection<C extends Record<any, any>>(name: string): Collection<C> | undefined {
+        return this.collections[name]
+    }
+
+    addCollection<C extends Record<any, any>>(name: string, opt?: CollectionInitOptions<C>) {
+        if (opt) {
+            if (!isArray(opt.indexes) || !opt.indexes.every(function (i) { return typeof i === 'string' }) || !isArray(opt.uniques) || !opt.uniques.every(function (i) { return typeof i === 'string' })) throw new SDBError("Type", "Indexes needs to be an array of strings")
+        }
+        return this.collections[name] = new Collection(name, this, opt ?? {})
+    }
+
+    loadCollection<C extends Record<any, any>>(coll: Collection<C> | CollectionJson<C>) {
+        if (!coll.name)
+            invalidName()
+        return this.collections[coll.name] = coll instanceof Collection ? coll : new Collection(coll.name, this).fromJSON(coll)
+    }
+
+    chain() { }
+
+    clearChanges() {
+        Object.values(this.collections).forEach(function (col) {
+            col.flushChanges()
+        })
+        this.emit("flushChanges", {})
+    }
+    toJSON(fast=true): {
+        name: string;
+        options: Partial<SpeedDbConstructorOptions> & SpeedDbOptions
+        collections: CollectionJson<any>[]
+    } {
+        return {
+            name: this.name,
+            options: this.options,
+            collections: Object.values(this.collections).map(coll => coll.toJSON(fast))
+        }
+    }
+    fromJSON({ name, options, collections }: {
+        name: string;
+        options: Partial<SpeedDbConstructorOptions> & SpeedDbOptions
+        collections: CollectionJson<any>[]
+    }) {
+        this.name = name
+        this.options = options
+        this.collections = {}
+        collections.forEach(coll => this.collections[coll.name] = new Collection(coll.name, this).fromJSON(coll))
+        this.emit("loaded", {})
+        return this
+    }
+    close(silent=false) {
+        Object.values(this.collections).forEach(function (col) {
+            col.close(silent)
+        })
+        this.collections = {}
+        this.emit("close", {})
+        this.clearAllEvents()
     }
 }
-
-export { SpeedDb }
