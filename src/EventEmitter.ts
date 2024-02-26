8214387d33a093a2184b38adb9eb0c92d64d2017
Praveen Yadav <praveenyadav7787@gmail.com>
2024-02-26 22:16:01 +0530

    feat(EventEmitter) Add EventEmitter class with on, off, emit and clearAllEvents methods
     - on: add/attach an event or array of events
     - off: remove an event or array of events
     - emit: fire an event with data
     -  clearAllEvents: remove all  attached functions from every event

---
+++ src/EventEmitter.ts
@@ -0,0 +1,57 @@
+import { hasOwn, isArray } from "./utils"
+import { DBError } from "./error"
+import { remove } from "@antfu/utils";
+
+type EventHandlers<E extends string> = Record<E, any>
+type EventFn<E extends keyof D, D> = ((event: E, data: D[E]) => void)
+
+
+export class EventEmitter<E extends string, EventCol extends EventHandlers<E>> {
+    events: Record<E, EventFn<E, EventCol>[]>
+
+    constructor(events: readonly E[]) {
+        this.events = {} as typeof this.events
+        if (!isArray<E>(events)) throw new DBError('Value', 'Events must be string array')
+
+        events.forEach(e => {
+            this.events[e] = []
+        })
+    }
+    on<Ev extends E, Handler extends EventFn<Ev, EventCol>>(event: Ev | Ev[], fn: Handler) {
+        if (!isArray(event))
+            event = [event]
+
+        event.forEach(ev => {
+            // does have the property
+            if (!hasOwn(this.events, ev))
+                throw new DBError("Event", "Event not available")
+
+            this.events[ev].push(fn as any)
+        })
+        return fn
+    }
+    off<Ev extends E, Handler extends EventFn<Ev, EventCol>>(event: Ev | Ev[], fn: Handler) {
+        if (!isArray(event))
+            event = [event]
+
+        event.forEach(ev => {
+            var fns = this.events[ev];
+            // below remove function will find index and remove it
+            if (fns) remove(fns, fn as any);
+        })
+    }
+    clearAllEvents() {
+        Object.keys(this.events).forEach(ev => {
+            if (hasOwn(this.events, ev))
+                this.events[ev as E] = []
+        })
+    }
+
+    emit<Ev extends E, Data extends EventCol[Ev]>(event: Ev, arg: Data) {
+        if (!hasOwn(this.events, event)) return
+        let len = this.events[event].length
+        for (let i = 0; i < len; i++) {
+            this.events[event][i](event, arg)
+        }
+    }
+}
